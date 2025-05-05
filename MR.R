aeb977ffa1b10db9be21db497654bc674283c19b
Ying Zhang <ying.z@rutgers.edu>
2025-05-05 00:57:56 -0400

    pre2

--- MR.R
+++ MR.R
@@ -35,6 +35,15 @@ head(dat_raw)
 dat_raw$q1_per <- dat_raw$q1_time / 20
 dat_raw$q1_logit <- log((dat_raw$q1_per)/(1-dat_raw$q1_per))
 
+dat_raw |> 
+  ggplot() +
+  aes(x = sentence, y = q1_logit, fill = survey) +
+  geom_boxplot() 
+
 View(dat_raw)
+
 mod1<- lm(q1_logit ~ survey * sentence, data = dat_raw)
 summary(mod1)
+
+dat_raw |> 
+ relocate(q1_per, q1_logit, .after = sentence)
